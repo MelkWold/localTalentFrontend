e226e8598c5d3517381382ef2038f2ddd573b2ef
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-10-15 21:45:32 -0400

    Registration form and css added

--- src/components/Authenication/Register.jsx
+++ src/components/Authenication/Register.jsx
@@ -1,9 +1,79 @@
-export default function Register(){
+import {useState} from 'react'; 
+import './Register.css';
 
-    return(
-        <>
-            <h1>Register</h1>
-            <h2>Welcome to Local Talent matcher. We aspire to connect talented and skilled individual in local communities with customers. Register now to start earning!</h2>
-        </>
-    )
+export default function Register() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        services: '',
+        role: 'client',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Get form data from the form
+        const form = e.target;
+        const formData = newFormData(form);
+
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const services = formData.get("services")
+        const role = formData.get("role");
+    
+        const allData = { 
+            email,
+            password,
+            services,
+            role,
+        };
+
+        console.log('Form data', allData);
+    };
+    
+
+    return (
+        <section className = "signupContainer">
+            <h1>Signup</h1>
+            <form className="signUpForm" onSubmit = {handleSubmit}>
+                <div className= "form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input id="email" type="email" name="email" placeholder ="person@email.com" required />
+                </div>
+                
+
+                <div className= "form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input id="password" type="password" name="password" required />
+                </div>
+
+
+                <div className= "form-group">
+                    <label htmlFor="services">Services: </label>
+                    <textarea id="services" name="services" placeholder='List your services separated by comma'/>
+                </div>
+
+
+                <fieldset>
+                    <legend>Role: </legend>
+                    <label>
+                        <input type="radio" name="role" value= "Provider" />
+                        Provider
+                    </label>
+                    <label>
+                        <input type="radio" name="role" value= "Customer" />
+                        Customer
+                    </label>
+                    <label>
+                        <input type="radio" name="role" defaultChecked={true} value= "both" />
+                        Provider and Customer
+                    </label>
+                </fieldset>
+
+                <br/><br/>
+                <button>Submit</button>
+
+            </form>
+        </section>
+)
 }
